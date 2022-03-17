39e5eda3c4c526f884fe1f2294c11f85f9f32ebe
nedu'm <davidsonchinedum@gmail.com>
2022-03-17 17:56:31 +0100

    added scope.js

---
+++ scope.js
@@ -0,0 +1,12 @@
+var teacher = 'Kyle';
+
+function otherClass() {
+  teacher = 'Getlify';
+  topic = "You don't know JS";
+  console.log("Welcome to Frontend Masters");
+}
+
+otherClass();
+
+teacher;
+topic;
