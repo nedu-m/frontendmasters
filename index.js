afefd53f1e70ee6ad7d4c0b11e4ed1ecffbe480e
nedu'm <davidsonchinedum@gmail.com>
2022-04-14 18:33:18 +0100

    currying, closure, composure

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//Used underscore.js to implement the underscore methods
 var _ = require('underscore');
 
-//_.MAP EXERCISE
+//Setting console.log function
+const log = (args) => console.log(args)
 
+//_.MAP EXERCISE
 _.map = function(list, callback) {
   var storage = [];
 
@@ -16,11 +19,11 @@ _.map = function(list, callback) {
   return storage;
 }
 
-var s = _.map([1,2,3], function(val) {
+var mapper = _.map([1,2,3], function(val) {
   return val + 1;
 })
 
-s;
+//mapper;
 
 _.filter = function(arr, callback) {
   //create a new storage
@@ -30,7 +33,7 @@ _.filter = function(arr, callback) {
     //check if callback returns true
     if (callback(arr[i], i, arr) === true) {
     //push into the arr
-     suspects.push(arr[i])
+      suspects.push(arr[i])
     }
   }
   //return arr
@@ -116,37 +119,166 @@ var listSuspects = _.filter(videoData, function(suspectObject) {
     return suspectObject.present
 })
 
-listSuspects;
+//listSuspects;
 
 const theSuspects = _.map(listSuspects, sus => {
   return sus.name;
 })
 
-theSuspects;
+//theSuspects;
 
 //Tuple - two arrays that contains two values
 const createTuple = (a, b, c, ...d) => {
   return [[a,c], [b,d]];
 }
 
-createTuple('it', 'be', 'could', 'anyone', 'no one');
+//createTuple('it', 'be', 'could', 'anyone', 'no one');
 
-createTuple;
+//createTuple;
 
 //Array-like Objects
 const constructArr = function() {
-  debugger
   const arr = Array.from(arguments);
   arr.push('the billards room');
   return arr.join(' ');
 };
-constructArr('was', 'it', 'in')
+//constructArr('was', 'it', 'in')
 
-constructArr;
+//constructArr;
 
 //High-order functions
 inc = n => n + 1
 
 sq = n => n * n
 
-solve = (n, func) => {return func(n)}
+//solve = (n, func) => { return func(n) }
+
+
+const ifElse = (condition, isTrue, isFalse, ...args) => {
+  return condition ? isTrue(...args) : isFalse(...args);
+}
+//ifElse(true, console.log, console.log, 'it', 'be', 'could', 'anyone', 'no one');
+
+//Reduce implementation
+const reduce = function (list, cb, initial = list[0]) {
+  let accumulator = initial;
+  for (let i = 0; i < list.length; i++) {
+    accumulator = cb(list[i], accumulator);
+  }
+  return accumulator;
+}
+
+//reduce([1,2,3,4], (acc, val) => acc + val);
+
+//Closure
+const myAlert = () => {
+  const x = 'Found the clue!';
+  let count = 0;
+  const alerter = () => {
+    console.log(`${x} ${++count}`);
+  };
+
+  return alerter;
+  //setTimeout(alerter, 1000);
+  //console.log('I found the clue!');
+};
+
+//myAlert();
+const funcalert = myAlert();
+//const funcalert2 = myAlert();
+//funcalert(); // I found the clue! 1
+
+const newClue = (suspect) => {
+  const length = suspect.length;
+
+  return (weapon) => {
+    let clue = length + weapon.length;
+    return !!(clue % 1);
+  };
+};
+
+const suspectUsed1 = newClue('Rusty');
+//suspectUsed1('Knife');
+
+function countClues() {
+  let n = 0;
+  return {
+    count: () => n++,
+    reset: () => n = 0
+  };
+};
+
+counter = countClues();
+counter1 = countClues();
+//log(counter.count());
+//log(counter1.count());
+
+
+const findSomeone = () => {
+  const speak = () => {
+    log(who);
+  };
+  let who = 'Mrs. White';
+
+  speak();
+};
+
+const someoneSpeaks = findSomeone;
+//log(someoneSpeaks());
+
+const makeTimer = () => {
+  let timeElapsed = 0;
+  
+  const stopWatch = () => { return timeElapsed };
+
+  const increased = () => timeElapsed++;
+
+  setInterval(increased, 1000);
+
+  return stopWatch;
+};
+
+let timer = makeTimer();
+//timer()
+
+//Currying
+const curry = (fn) => {
+  return (arg) => {
+    return (arg2) => {
+      return (arg3) => {
+        return fn(arg, arg2, arg3);
+      } 
+    }
+  }
+}
+
+var abc = function (a, b, c) {
+  return [a, b, c];
+};
+
+var curried = curry(abc)
+
+//log(curried(1)(2)(3))
+
+//Compose
+const compose = (fn, fn2) => {
+  return (arg) => {
+    const result = fn2(arg);
+    return fn(result);
+  };
+};
+
+const consider = (name) => {
+  return `${name} is a suspect`;
+};
+
+const exclaim = (statement) => {
+  return `${statement.toUpperCase()}!`;
+};
+
+const blame = compose(consider, exclaim);
+
+// blame('Rusty');
+
+
+
